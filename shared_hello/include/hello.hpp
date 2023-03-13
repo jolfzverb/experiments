b43f314b6efb3e0774bd03a0898874ed7b11e1e1
Yury Usishchev <jolfzverb@yandex-team.ru>
2023-03-13 15:07:28 +0300

    add hello lib

---
+++ shared_hello/include/hello.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace hello_world {
+
+// greets a user by writing something to cout
+void HelloWorld();
+
+}
---
+++ shared_hello/src/hello.cpp
@@ -0,0 +1,11 @@
+#include "include/hello.hpp"
+
+#include <iostream>
+
+namespace hello_world {
+
+void HelloWorld(){
+  std::cout << "Hello user!" << std::endl;
+}
+
+}
